d1425035b60d62528bb2aef0a4d8a98b5082c3c0
Laura Atencio <lauraatenciob@gmail.com>
2024-01-10 11:09:14 -0500

    random quote logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+import generateRandomQuote from "./generateRandomQuote"
 
 function App() {
+  const [quote, setQuote] = useState(generateRandomQuote());
+
+  function handleClick() {
+    setQuote(generateRandomQuote())
+  }
+
   return (
-    <h1>Random Quote Machine</h1>
+    <main>
+      <section id="quote-box">
+        <p id="text">
+          {quote.text}
+        </p>
+        <p id="author">- {quote.author}</p>
+        <button onClick={handleClick} id="new-quote">New quote</button>
+        <a id="tweet-quote" target="_blank" rel="noreferrer" href={`https://twitter.com/intent/tweet?text=${quote.text}`}>Tweet</a>
+      </section>
+    </main>
   );
 }
 
